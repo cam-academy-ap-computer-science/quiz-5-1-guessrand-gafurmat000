f3a7a1ec0154a07206a7e0583553d12715ff980a
gafurmat000 <gafurmat000@CAM-LAB-17.bgsd.k12.wa.us>
2019-11-15 07:37:41 -0800

    Quiz complete.

--- src/GuessRand.java
+++ src/GuessRand.java
@@ -2,32 +2,37 @@ import java.util.*;
 public class GuessRand {
 
 	public static void main(String[] args) {
-		int rand = 0;
+		int rand;
 		boolean success = false;
-		// Instantiate (create) a new Random() object 'r'
-		rand = // Get a random number between 1-100
+		Random r = new Random();// Instantiate (create) a new Random() object 'r'
+		int number = r.nextInt(100) + 1; // Get a random number between 1-100
 		
-		while (    // loop until 'success' is true. ) {
-			int guess = 0;
-			// Instantiate a new Scanner 'console'
+		while (success != true) {   // loop until 'success' is true. ) {
+			Scanner console = new Scanner(System.in);// Instantiate a new Scanner 'console'
 			System.out.println("Your Guess: ?");
-			guess = // Get an integer from the user.
-			success =  // call 'compare()' method
+			int guess = console.nextInt(); // Get an integer from the user.
+			 success = compare(guess, number);  // call 'compare()' method
 		}
+		
 	}
+	
+	
 	
 	// Declare a method named 'compare()' that accepts two parameters 'guess' and 'rand' and returns
 	// a boolean.
-	public static   
+	public static boolean compare(int guess, int rand) {
 		boolean got_it = false;
-		if () {  // Fix if test
+		if (guess == rand) {  // Fix if test
 			System.out.println("You got it!");
 			got_it = true;
-		} else if () {  // Fix if test
+		} else if (guess < rand) {  // Fix if test
 			System.out.println("No, it's Higher");
 		} else {
 			System.out.println("No, it's Lower");
 		}
 		return got_it;
 	}
+
 }
+	
+
